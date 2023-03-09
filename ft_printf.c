731f9a68bc708e8c46a52c365667db9d56410906
carlos <cfiestas6@gmail.com>
2023-03-09 15:29:02 +0100

    main function

--- ft_printf.c
+++ ft_printf.c
@@ -10,7 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./ft_printf.h"
+
 int	ft_printf(const char *str, ...)
 {
+	va_list	args;
+	int	count;
+	int	i;
 
+	count = 0;
+	i = 0;
+	if (!str)
+		return (0);
+	va_start(args, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			count += ft_printf_aux(str, &i, args);
+		}
+		else
+		{
+			ft_putchar(str[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
